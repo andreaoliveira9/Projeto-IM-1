e577b78e82118cfbaed7157bdb5dd9926d84cba4
André Oliveira <andreaoliveira@ua.pt>
2024-10-29 19:09:28 +0000

    button play/pause, input search

--- app/mapping.py
+++ app/mapping.py
@@ -12,62 +12,10 @@ class MapObject:
 class Buttons(MapObject):
     @property
     def play(self):
-        return self.find_element("//button[@aria-label='Play']")
-
-    @property
-    def pause(self):
-        return self.find_element("//button[@aria-label='Pause']")
-
-    @property
-    def next(self):
-        return self.find_element("//button[@aria-label='Next']")
-
-    @property
-    def previous(self):
-        return self.find_element("//button[@aria-label='Previous']")
-
-    @property
-    def volume_up(self):
-        return self.find_element("//button[@aria-label='Volume up']")
-
-    @property
-    def volume_down(self):
-        return self.find_element("//button[@aria-label='Volume down']")
-
-    @property
-    def mute(self):
-        return self.find_element("//button[@aria-label='Mute']")
-
-    @property
-    def unmute(self):
-        return self.find_element("//button[@aria-label='Unmute']")
-
-    @property
-    def repeat(self):
-        return self.find_element("//button[@aria-label='Repeat']")
-
-    @property
-    def shuffle(self):
-        return self.find_element("//button[@aria-label='Shuffle']")
-
-    @property
-    def search(self):
-        return self.find_element("//input[@id='input']")
-
-    @property
-    def first_song_result(self):
-        return self.find_element(
-            "(//ytmusic-playlist-similar-songs-renderer)[1]//ytmusic-responsive-list-item-renderer[1]"
-        )
-
-    @property
-    def first_playlist_result(self):
-        return self.find_element(
-            "(//ytmusic-playlist-similar-songs-renderer)[1]//ytmusic-playlist-renderer[1]"
-        )
+        return self.find_element(By.XPATH, "//button[@aria-label='Reproduzir']")
 
 
 class Inputs(MapObject):
     @property
     def search(self):
-        return self.find_element("//input[@id='input']")
+        return self.find_element(By.XPATH, "//*[@id='input']")
